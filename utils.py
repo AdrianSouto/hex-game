ab91a29769c6d9775e2081c90fd303e309573150
Adrian <adrianalejandrosm03@gmail.com>
2025-04-09 20:44:15 -0400

    new time decorator

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import time
+
+
 numbers = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
 
 
@@ -18,3 +21,13 @@ player_symbol = {
     1: "🔵",
     2: "🔴",
 }
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function '{func.__name__}' executed in {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
